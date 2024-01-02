ce0af3507655574905913845737ef0b1e40b4a11
ismael <spacingismael@gmail.com>
2024-01-03 00:10:40 +0100

    cycle task

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,7 +3,24 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * check_cycle - function checks is there a cycle
+ * @list: pointer to list 'struct'
+ * Return: 1 if cyclical, 0 otherwise
+ */
 int check_cycle(listint_t *list)
 {
+	listint_t *slow, *fast;
 
+	fast = list->next;
+	slow = list;
+
+	while (fast != NULL && fast->next != NULL)
+	{
+		if (fast == slow)
+			return (1);
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
 }
