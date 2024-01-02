fd224001fd0cf1ff34fd511de6993e06ef9f4e91
ismael <spacingismael@gmail.com>
2024-01-03 00:12:57 +0100

    cycle task2

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -12,15 +12,15 @@ int check_cycle(listint_t *list)
 {
 	listint_t *slow, *fast;
 
-	fast = list->next;
+	fast = list;
 	slow = list;
 
 	while (fast != NULL && fast->next != NULL)
 	{
-		if (fast == slow)
-			return (1);
 		slow = slow->next;
 		fast = fast->next->next;
+		if (slow == fast)
+			return (1);
 	}
 	return (0);
 }
