7036d37dd89532afad0559c23823409f826256a1
ismael <spacingismael@gmail.com>
2024-01-06 12:44:18 +0100

    fixing task 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,21 +1,18 @@
 #include "lists.h"
-#include <stdio.h>
-#include <stddef.h>
 
 /**
- * list_len - function that returns the number of elements
- * in a linked 'listint_t' list
- * @hh: pointer to struct node
- * Return: returns number of elements
+ * list_len - function that counts the length of a linked list
+ * @h: ptr to linked list
+ * Return: returns the length of linked list
  */
-size_t list_len(listint_t *hh)
+size_t list_len(const listint_t *h)
 {
 	int i = 0;
 
-	while (hh != NULL)
+	while (h != NULL)
 	{
 		i++;
-		hh = hh->next;
+		h = h->next;
 	}
 	return (i);
 }
@@ -23,27 +20,35 @@ size_t list_len(listint_t *hh)
 /**
  * is_palindrome - function checks if a singly linked list is a
  * palindrome
- * @head: list
- * Return: returns 0 if it's not a palindrome, otherwise 1
+ * @head: ptr to ptr to linked list
+ * Return: returns 1 if palindrome, otherwise 0
  */
-int is_palindrome(listint_t **head)
+ int is_palindrome(listint_t **head)
 {
 	listint_t *temp = *head;
-	int arr[list_len(temp)];
+	int *arr;
 	int i = 0, j;
 
 	if (*head == NULL)
 		return (1);
-	while (temp)
+	arr = malloc(sizeof(int) * list_len(temp));
+	if (!arr)
+		return (1);
+
+	while(temp)
 	{
 		arr[i] = temp->n;
 		temp = temp->next;
 		i++;
 	}
-	for (j = 0; j < (i / 2); j++)
+	for (j = 0; j < (i/2); j++)
 	{
 		if (arr[j] != arr[i - j - 1])
+		{
+			free(arr);
 			return (0);
+		}
 	}
+	free(arr);
 	return (1);
 }
