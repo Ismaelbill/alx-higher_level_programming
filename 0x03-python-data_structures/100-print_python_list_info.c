86d9fe1cab0c1ab60eaeeb8c5f4588649383e64e
ismael <spacingismael@gmail.com>
2024-01-06 15:39:40 +0100

    advanced task in 0x03..

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <Python.h>
+/**
+ * print_python_list_info - prints info about python list
+ * @p: PyObject list
+ */
+
+void print_python_list_info(PyObject *p)
+{
+	int size, allocated, i;
+	PyObject *objct;
+
+	size = Py_SIZE(p);
+	allocated = ((PyListObject *)p)->allocated;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", allocated);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
+		objct = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(objct)->tp_name);
+	}
+}
