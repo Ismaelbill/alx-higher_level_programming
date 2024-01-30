1d82a67bfd8336916682da1f4eb5c0c730f12464
ismael <spacingismael@gmail.com>
2024-01-30 02:06:49 +0100

    task 9 in 08

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -14,42 +14,6 @@ class Rectangle:
         self.width = width
         Rectangle.number_of_instances += 1
 
-    @property
-    def width(self):
-        """ property to retrieve it """
-        return self.__width
-
-    @width.setter
-    def width(self, value):
-        """ property setter to set it """
-        if type(value) is not int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
-    @property
-    def height(self):
-        """ property to retrieve it """
-        return self.__height
-
-    @height.setter
-    def height(self, value):
-        """ property setter to set it """
-        if type(value) is not int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
-
-    def area(self):
-        return self.height * self.width
-
-    def perimeter(self):
-        if (self.__height == 0) or (self.__width == 0):
-            return 0
-        return 2 * (self.__height + self.__width)
-
     def __str__(self):
         """ print the rectangle with the character # """
 
@@ -81,4 +45,40 @@ class Rectangle:
     def square(cls, size=0):
         """ instantiate a new square """
 
-        return Rectangle(size, size)
+        return cls(size, size)
+
+    @property
+    def width(self):
+        """ property to retrieve it """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ property setter to set it """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ property to retrieve it """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ property setter to set it """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        return self.__height * self.__width
+
+    def perimeter(self):
+        if (self.__height == 0) or (self.__width == 0):
+            return 0
+        return 2 * (self.__height + self.__width)
