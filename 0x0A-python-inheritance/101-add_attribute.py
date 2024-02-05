2e241cbe70161dd370dbbc14059fe78f61e231ae
ismael <spacingismael@gmail.com>
2024-02-06 00:39:49 +0100

    task 101

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" MODULE """
+
+
+def add_attribute(obj, name, value):
+    """ function that a new attribute to an object if possible """
+    if not hasattr(obj, '__dict__'):
+        raise TypeError("can't add new attribute")
+    obj.name = value
