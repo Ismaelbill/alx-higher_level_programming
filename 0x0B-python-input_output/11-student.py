ca80b8e2ad8591e12035cbc505b1120c6de5b3d4
ismael <spacingismael@gmail.com>
2024-02-14 00:13:10 +0100

    task 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" Student to JSON with filter """
+
+
+class Student:
+    """ cls student """
+    def __init__(self, first_name, last_name, age):
+        """ initialize data student """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ dictionary representation """
+        if attrs is None:
+            return self.__dict__
+        else:
+            diction = {}
+            for i in attrs:
+                if i == 'first_name':
+                    diction['first_name'] = self.first_name
+                if i == 'last_name':
+                    diction['last_name'] = self.last_name
+                if i == 'age':
+                    diction['age'] = self.age
+            return diction
+
+        def reload_from_json(self, json):
+            """ replaces all attributes """
+            for key, val in json.items():
+                self.key = val
