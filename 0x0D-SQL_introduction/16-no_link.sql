50410db7d72df5854787481b10704f025b98c696
root <root@TENET>
2024-03-14 01:45:41 +0000

    task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,5 @@
 -- ists all records of the table second_table of the database hbtn_0c_0
+DELETE FROM second_table WHERE name = "George";
+INSERT INTO second_table VALUES (5, "Aria", 18);
+INSERT INTO second_table VALUES (6, "Aria", 12);
 SELECT score, name FROM second_table ORDER BY score DESC;
