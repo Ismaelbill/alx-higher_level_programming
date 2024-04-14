2bd11ca7eb11698cab66c4e0ef0dc3b14d722b15
root <root@TENET>
2024-04-14 09:29:16 +0100

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" script that takes in an argument and displays all values in
+    the states table of hbtn_0e_0_usa where name matches the argument
+"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+
+    args = sys.argv
+
+    conn = MySQLdb.connect(host="localhost", port=3306, user=args[1],
+                           passwd=args[2], db=args[3], charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY\
+                '{}' ORDER BY id ASC".format(args[4]))
+
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+
+    cur.close()
+
+    conn.close()
