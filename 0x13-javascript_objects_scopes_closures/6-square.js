12ff45ca838fc121eacbabf75ee4c34264056fdf
ismael <spacingismael@gmail.com>
2024-05-10 13:24:32 +0100

    last tasks

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,1 +1,16 @@
 #!/usr/bin/node
+
+const Rectangle = require('./4-rectangle.js');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    const size = this.height;
+    if (c === undefined) { c = 'X'; }
+    for (let i = 0; i < size; i++) console.log(c.repeat(size));
+  }
+}
+module.exports = Square;
