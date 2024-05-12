ce3f94f2473c4fb20121a656328775b0c21d219b
ismael <spacingismael@gmail.com>
2024-05-12 09:39:11 +0100

    task 102 fixed

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 
 /* script that concats 2 files.
- * 
+ *
  * The first argument is the file path of the first source file
  * The second argument is the file path of the second source file
  * The third argument is the file path of the destination
  */
 const fs = require('fs');
-let argv = process.argv;
+const argv = process.argv;
 fs.readFile(argv[2], (err, dataf) => {
-   if (err) throw err;
+  if (err) throw err;
 
-   fs.readFile(argv[3], (err, datas) => {
+  fs.readFile(argv[3], (err, datas) => {
+    if (err) throw err;
+
+    fs.writeFile(argv[4], dataf + datas, (err) => {
       if (err) throw err;
-
-      fs.writeFile(argv[4], dataf + datas, (err) => {
-         if (err) throw err;
-      });
-   });
+    });
+  });
 });
