b3cb4c7b2bd89e112ca3c0b21f704c18c5971722
ismael <spacingismael@gmail.com>
2024-05-12 10:58:38 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,16 @@
 #!/usr/bin/node
 
+const argv = process.argv.slice(2); let max;
 
+function secondBig (arr) {
+  if (arr.length === 0 || arr.length === 1) {
+    console.log(0);
+  } else {
+    arr = arr.map((el) => (el * 1));
+    max = Math.max(...arr);
+    arr.splice(arr.indexOf(max), 1);
+    console.log(Math.max(...arr));
+  }
+}
+
+secondBig(argv);
