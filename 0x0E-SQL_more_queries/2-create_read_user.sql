cd6aa8b18069f7663487a72265020d3f3b0fe771
ismael <spacingismael@gmail.com>
2024-05-14 16:46:45 +0100

    task 2 fixed

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,7 +1,4 @@
 -- script that creates the database hbtn_0d_2 and the user user_0d_2
-CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_2'@'localhost' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-USE hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON  hbtn_0d_2.* TO 'user_0d_2'@'localhost';
