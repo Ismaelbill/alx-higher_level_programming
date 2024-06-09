cb10116a943d518ae26d7066f50a8647564e0f2e
ismael <spacingismael@gmail.com>
2024-06-09 15:52:03 +0100

    deleting projects

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const strArr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (const char of strArr) {
-  console.log(char);
-}
--- 0x13-javascript_objects_scopes_closures/100-map.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-const list = require('./100-data').list;
-let i = 0;
-const newList = list.map((x) => x * i++);
-console.log(list);
-console.log(newList);
