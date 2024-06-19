defbccf991a0f03c55153b909c892427c04234d9
ismael <spacingismael@gmail.com>
2024-06-19 01:58:49 +0100

    js warm up

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const strC = 'C is fun';
+
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 0; i < parseInt(process.argv[2]); i++) {
+    console.log(strC);
+  }
+}
