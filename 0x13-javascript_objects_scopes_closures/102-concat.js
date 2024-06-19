35e13ddcc2faf4c6fdfa7cc5a254ee27b8156d6a
ismael <spacingismael@gmail.com>
2024-06-19 20:28:46 +0100

    js second dir

---
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+/* script that concats 2 files.
+ *
+ * The first argument is the file path of the first source file
+ * The second argument is the file path of the second source file
+ * The third argument is the file path of the destination
+ */
+const fs = require('fs');
+const argv = process.argv;
+fs.readFile(argv[2], (err, dataf) => {
+  if (err) throw err;
+
+  fs.readFile(argv[3], (err, datas) => {
+    if (err) throw err;
+
+    fs.writeFile(argv[4], dataf + datas, (err) => {
+      if (err) throw err;
+    });
+  });
+});
