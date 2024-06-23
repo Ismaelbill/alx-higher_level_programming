14117dbbba220cc97e7736c3091cd29eeeaecf14
ismael <spacingismael@gmail.com>
2024-06-23 01:17:54 +0100

    task 2, 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""  script that takes in an argument and displays
+all values in the states table of hbtn_0e_0_usa
+where name matches the argument."""
+
+if __name__ == "__main__":
+
+    import MySQLdb
+    import sys
+
+    argv = sys.argv
+    con = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                          passwd=argv[2], db=argv[3], charset="utf8")
+    cur = con.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s", (argv[4], ))
+
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    con.close()
