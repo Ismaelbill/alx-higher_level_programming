bdd2b1c24a34d0e74ea1d8d93159d1db08b58f39
ismael <spacingismael@gmail.com>
2024-07-04 16:36:33 +0100

    1-task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-status=$(curl -i -s -X GET $(1) | head -n 1 | cut -d " " -f2)
-
-if [ $status -eq 200 ]; then
-	curl -i -s -X GET $1
-fi
+curl -s -X GET $1 -L
