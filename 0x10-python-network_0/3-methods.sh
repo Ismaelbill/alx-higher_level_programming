88e09b4743ab1d6dc68386c8770c356b0426f49f
ismael <spacingismael@gmail.com>
2024-07-04 17:41:06 +0100

    1-task 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  script that takes in a URL and displays all HTTP methods the server will accept.
-curl -si -X OPTIONS 0.0.0.0:5000/route_4 | grep "Allow" | cut -d " " -f2-
+curl -si -X OPTIONS $1 | grep "Allow" | cut -d " " -f2-
