55339504a447f81b90570d224030da9f6865b852
ismael <spacingismael@gmail.com>
2024-07-06 00:04:41 +0100

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" script that takes in a URL, sends a request to the URL
+and displays the body of the response. """
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    r = request.get(sys.argv[1])
+    if r.status_code >= 400:
+        print('Error code: {}'.format(r.status_code))
+        exit()
+    print(r.text)
