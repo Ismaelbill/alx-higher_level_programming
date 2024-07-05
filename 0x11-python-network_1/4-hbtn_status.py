d2f1c55febaa00a903f90085b24e16650942fd49
ismael <spacingismael@gmail.com>
2024-07-05 23:00:29 +0100

    task 4

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status """
+
+if __name__ == "__main__":
+    import requests
+
+    var = "https://alx-intranet.hbtn.io/status"
+    r = requests.get(var)
+    print("Body response:")
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- type: {}".format(r.text))
