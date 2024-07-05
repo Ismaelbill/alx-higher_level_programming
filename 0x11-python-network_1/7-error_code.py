36949c92ba1dc7d17446f532d0d25f3602575048
ismael <spacingismael@gmail.com>
2024-07-06 00:18:21 +0100

    task 7#3

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     import sys
     import requests
 
-    r = request.get(sys.argv[1])
+    r = requests.get(sys.argv[1])
 
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
