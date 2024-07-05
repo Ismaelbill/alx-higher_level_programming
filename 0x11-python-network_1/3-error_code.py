b0540518bec0f457663dfedcabfd3d875411cc27
ismael <spacingismael@gmail.com>
2024-07-05 22:38:03 +0100

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" sends a request to the URL and displays the body of the response """
+
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.error
+    import sys
+
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as f:
+            print(f.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
