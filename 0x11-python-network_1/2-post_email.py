cd93453c0dbbf5b75422d2c1d82c39fd987648e8
ismael <spacingismael@gmail.com>
2024-07-05 21:19:35 +0100

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" ends a POST request to the passed URL with the email
+as a parameter, and displays the body of the response """
+
+if __name__ == "__main__":
+    import urllib.parse
+    import urllib.request
+    import sys
+
+    data = urllib.parse.urlencode({'email': sys.argv[2]})
+    data = data.encode('ascii')
+    with urllib.request.urlopen(sys.argv[1], data) as f:
+        print(f.read().decode('utf-8'))
