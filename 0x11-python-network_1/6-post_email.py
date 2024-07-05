81ebc24038ab90b41390954dd5658973929baed9
ismael <spacingismael@gmail.com>
2024-07-05 23:43:30 +0100

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+""" script that takes in a URL and an email address,
+sends a POST request to the passed URL with the email as a parameter
+"""
+
+if __name__ == "__main__":
+    import sys
+    import requests
+
+    data = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], data=data)
+    print(r.text)
