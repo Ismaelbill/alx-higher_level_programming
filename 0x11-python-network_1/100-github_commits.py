087186c948a9d09105d9cf9d9e3ed59858b1dc25
ismael <spacingismael@gmail.com>
2024-07-07 13:47:28 +0100

    task 100#1

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """ listing 10 commits """
 
-import requests
-import sys
+if __name__ == "__main__":
+    import requests
+    import sys
 
-var = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1], sys.argv[2])
+    var = "https://api.github.com/repos/{}/{}/commits".format(sys.argv[1], sys.argv[2])
 
-r = requests.get(var)
+    r = requests.get(var)
 
-j = 0
-for i in list(r.json()):
-    print(i['sha'], i['commit']['author']['name'])
-    j += 1
-    if j == 10:
-        exit(0)
+    j = 0
+    for i in list(r.json()):
+        print(i['sha'], i['commit']['author']['name'])
+        j += 1
+        if j == 10:
+            exit(0)
