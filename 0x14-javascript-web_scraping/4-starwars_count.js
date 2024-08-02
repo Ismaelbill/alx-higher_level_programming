6f2cc779a5886d85f872cdbd1127c2bfea0ff6fe
ismael <spacingismael@gmail.com>
2024-08-02 22:28:29 +0100

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+request(process.argv[2], function (_, res, body) {
+  const data = JSON.parse(body);
+  const character = 'https://swapi-api.alx-tools.com/api/people/18/';
+  let j = 0;
+  for (const i of data.results) {
+    for (const k of i.characters) {
+      if (character === k) { j++; }
+    }
+  }
+  console.log(j);
+});
