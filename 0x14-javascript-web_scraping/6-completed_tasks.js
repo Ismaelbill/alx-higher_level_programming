bb93141deb0344286c1c7add01b8bb9fb0f4bd42
ismael <spacingismael@gmail.com>
2024-08-03 00:13:52 +0100

    fixing task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,16 +6,9 @@ request(process.argv[2], function (_, res, body) {
   const data = JSON.parse(body);
 
   const obj = {};
-  let j = 0;
-  let i = 1;
-  for (i; i <= 10; i++) {
-    j = 0;
-    for (const user of data) {
-      if (user.userId === i) {
-        if (user.completed === true) { j++; }
-      }
-    }
-    obj[`${i}`] = j;
+  for (const user of data) {
+    if (obj[user.userId] === undefined) { obj[user.userId] = 0; }
+    if (user.completed) { obj[user.userId]++; }
   }
-  console.log(JSON.stringify(obj));
+  console.log((obj));
 });
