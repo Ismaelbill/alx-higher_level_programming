29f47841695719a3f27b79604cdf427d1c7a99b0
ismael <spacingismael@gmail.com>
2024-08-02 22:52:29 +0100

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (_, res, body) {
+  try {
+    fs.writeFileSync(process.argv[3], body);
+  } catch (err) {
+    console.log(err);
+  }
+});
