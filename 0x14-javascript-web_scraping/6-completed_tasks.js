1a5b60d18012bfba17614d98b32578cd56e289fe
ismael <spacingismael@gmail.com>
2024-08-02 23:58:37 +0100

    task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+const request = require('request');
+
+request('https://jsonplaceholder.typicode.com/todos', function (_, res, body) {
+  const data = JSON.parse(body);
+
+  const obj = {};
+  let j = 0;
+  let i = 1;
+  for (i; i <= 10; i++) {
+    j = 0;
+    for (const user of data) {
+      if (user.userId === i) {
+        if (user.completed === true) { j++; }
+      }
+    }
+    obj[`${i}`] = j;
+  }
+  console.log(obj);
+});
